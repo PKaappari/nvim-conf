a7e4644b40e000a79781ab8c5e0892b2fa9e2993
Panu Paakkari <panu.paakkari@outlook.com>
2024-03-15 09:56:14 +0200

    setup keymaps

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -22,9 +22,9 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         -- null_ls.builtins.diagnostics.eslint,
-        -- null_ls.builtins.code_actions.eslint,
+        null_ls.builtins.code_actions.eslint,
         null_ls.builtins.formatting.prettierd,
-        -- null_ls.builtins.code_actions.refactoring,
+        null_ls.builtins.code_actions.refactoring,
         null_ls.builtins.diagnostics.tsc,
       },
     })
